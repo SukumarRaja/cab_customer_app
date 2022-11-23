1a286ab24c047b734a3a6aeffdb165e12948790d
sukumar <108935834+Limitless360@users.noreply.github.com>
2022-11-23 19:37:48 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/package/package/package/MainActivity.kt
@@ -0,0 +1,7 @@
+package hashseven.com.customer
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
